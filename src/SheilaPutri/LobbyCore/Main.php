d382699b0da0164697a43720efb57c71704e0087
Sheila Putri <86517792+SheilaPutri@users.noreply.github.com>
2021-06-26 22:56:11 +0700

    Update Main.php

--- src/SheilaPutri/LobbyCore/Main.php
+++ src/SheilaPutri/LobbyCore/Main.php
@@ -1,1 +1,15 @@
-Main.php es
+<?php
+
+namespace SheilaPutri\LobbyCore;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener{
+
+    public function onEnable(){
+     $this->getServer()->getLogger()->Info("Plugin ini dibuat oleh Sheila Putri Aprilia");
+     $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+}

f8ec4da5a824c5c272e2aa1dc4c5abe31f9ec653
Sheila Putri Aprilia <86517792+SheilaPutri@users.noreply.github.com>
2021-06-28 23:35:00 +0700

    Update Main.php

--- src/SheilaPutri/LobbyCore/Main.php
+++ src/SheilaPutri/LobbyCore/Main.php
@@ -4,12 +4,17 @@ namespace SheilaPutri\LobbyCore;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
 
 class Main extends PluginBase implements Listener{
 
     public function onEnable(){
      $this->getServer()->getLogger()->Info("Plugin ini dibuat oleh Sheila Putri Aprilia");
      $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onPlayerJoin(PlayerJoinEvent $e){
+     $p = $e->getPlayer();
     }
 
 }
